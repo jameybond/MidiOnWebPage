50a61d4729e64f7ef62de1743b6f37ecf5629057
jameybond <jameybond@gmail.com>
2019-07-13 23:46:28 +0100

    Added Function and self executing items

--- injectMidiChord(NotMidiEnableFunction).js
+++ injectMidiChord(NotMidiEnableFunction).js
@@ -1,4 +1,5 @@
-var chordcount = document.getElementsByClassName("chord-pro-note").length;
+(function() {
+    var chordcount = document.getElementsByClassName("chord-pro-note").length;
 for ( var x=0 ; x < chordcount; x++)
     { var element = document.getElementsByClassName("chord-pro-note")[x];
         var Chordname = element.innerText;
@@ -9,3 +10,4 @@ if (Chordname != "") {
         var Suffix = element.getElementsByClassName("suffix")[0].innerHTML;
         if (Suffix == "")  {Suffix = 0};
     element.innerHTML = ' <button style="border:0.5px solid white; background-color: transparent;" onclick="noteOn(' + Chord +','+ Suffix +')">' + Chordname + '</button>';}}
+})();
