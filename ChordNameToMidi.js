ee410dd74985862dca797975e7913dde59d611ae
jameybond <jameybond@gmail.com>
2020-01-04 16:46:38 +0000

    Create ChordNameToMidi.js
    To Update the Chord names to a midi note on the sites like https://tabs.ultimate-guitar.com/ and Churchspot

---
+++ ChordNameToMidi.js
@@ -0,0 +1,40 @@
+(function(){
+var selectedElement = window.getSelection().anchorNode.parentNode;
+var selectedclass = selectedElement.className;
+//var buts = document.getElementsByClassName('_3bHP1 _3ffP6');
+
+var buts = document.getElementsByClassName(selectedclass);
+alert(buts);
+for (var i=0; i <buts.length; i++){
+  var chordcap = buts[i].innerText;
+
+  chordcap = chordcap.trim(); 
+  var chordary = chordcap.split("");
+  var chordarylen = chordary.length;
+  var chord = chordary[0];
+  var chordsuf = chordcap.slice(1);  
+  if((chordary[1] == "#") || (chordary[1] == "b")){chord = chord+chordary[1]; chordsuf= chordcap.slice(2)};
+  var chordtext = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+  var midinote = [ 60, 61,62,63,64,65,66,67,68,69,70,71];
+  var chordtype = ["m", "dim" , "sus"]
+  var chordnot2 = [-1,-1,1 ]
+  var chordnot3 = [0,-1,7]
+  var a = chordtext.indexOf(chord);
+
+  chord = chordtext[a]; 
+
+if ( chordsuf =="") {var c = 0; var d = 0 }  
+else {var c = chordnot2[chordtype.indexOf(chordsuf)]; var d = chordnot3[chordtype.indexOf(chordsuf)]};
+
+
+var ChordTransposed = "noteOn("+ (Number(midinote[a])) +","+ (Number(midinote[a])+4+c) + "," + (Number(midinote[a])+7+d) +")";
+
+
+
+buts[i].innerHTML = '<chordbutton style="border:0.5px solid white; background-color: transparent;" ontouchstart='+ChordTransposed +'>' + chord + chordsuf  + '</button>';
+//var new_elem = document.createElement(newchord);
+
+//buts[i].innerHTML = newchord;
+}
+
+)();
