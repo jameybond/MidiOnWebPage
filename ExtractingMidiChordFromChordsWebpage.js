e9226901984f2d5efdd6a83dd1b717c3b9682f74
jameybond <jameybond@gmail.com>
2021-01-10 12:33:20 +0000

    New Upload from tested Jsbin
    New Upload from tested Jsbin

--- ExtractingMidiChordFromChordsWebpage.js
+++ ExtractingMidiChordFromChordsWebpage.js
@@ -1,9 +1,10 @@
 (function(){
+      
 var chordclass = document.getSelection().focusNode.parentElement.className;
 var lyricandchordhtml = document.getSelection().anchorNode.parentElement.offsetParent;
 
-
-
+  
+  
 var chordcount = lyricandchordhtml.getElementsByClassName(chordclass).length;
 for ( var x=0 ; x < chordcount; x++) 
   {var element = lyricandchordhtml.getElementsByClassName(chordclass)[0]; 
@@ -21,10 +22,10 @@ var chordcap = element.innerText.trim();
         };
      var chordtext = ["C", "C#","Db", "D", "D#","Eb", "E", "F", "F#","Gb", "G", "G#","Ab", "A", "A#","Bb", "B"];
      var midinote = [ 60, 61,61,62,63,63,64,65,66,66,67,68,68,69,70,70,71];
-     var chordtype = ["m", "dim","sus","M7","m7","add9","7"];
-     var chordnot2 = [-1,-1,1,0,-1,0,0];
-     var chordnot3 = [0,-1,7,0,0,0,0];
-     var chordnot4 = [0,-1,7,-1,-2,2,-2];
+     var chordtype = ["m", "dim","sus","M7","m7","add9","7","2","4","aug","dim","7sus"];
+     var chordnot2 = [-1,-1,1,0,-1,0,0,-2,0,0,-1,3];/* increase or decrease to 4*/
+     var chordnot3 = [0,-1,7,0,0,0,0,0,0,+1,-1,4]; /* increase or decrease to 7*/
+     var chordnot4 = [0,-1,7,-1,-2,2,-2,0,-7,0,0,0]; /* increase or decrease to 12 */
      var chordnote1 = chordtext.indexOf(chord);
          chord = chordtext[chordnote1];
          chordnote1 = Number(midinote[chordnote1]); 
@@ -60,5 +61,10 @@ var att1 = document.createAttribute("style");
     att2.value = "this.select();";                          
     x.setAttributeNode(att2);
     
- document.body.appendChild(x);
-  })();
+  
+  
+/* document.body.appendChild(x); */
+document.body.insertBefore(x, document.body.childNodes[0]);
+
+})();
+
